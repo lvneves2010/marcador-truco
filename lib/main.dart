315292bf2b92de44716b0917bfd9bfd0829211b1
Leonardo <lvneves2010@gmail.com>
2020-06-10 20:07:00 -0300

    new buttons + decoration

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,64 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _counter2 = 0;
+  bool nosGanhamos = false;
 
   void _incrementCounterUs() {
     setState(() {
+      if (_counter < 12) {
       _counter++;
+      }
     });
   }
+
+  void _incrementTrucoUs() {
+    setState(() {
+      if (_counter < 12) {
+      _counter = _counter + 3;
+      }
+      if (_counter >= 12) {
+       _counter = 12;
+       nosGanhamos = true; 
+      }
+    });
+  }
+
+  void _decrementCounterUs() {
+    setState(() {
+      if (_counter > 0) {
+      _counter--;
+      }
+    });
+  }
+
   void _incrementCounterThem() {
     setState(() {
+      if (_counter2 < 12) {
       _counter2++;
+      }
     });
   }
+
+  void _incrementTrucoThem() {
+    setState(() {
+      if (_counter2 < 12) {
+      _counter2 = _counter2 + 3;
+      }
+      if (_counter2 >= 12) {
+       _counter2 = 12;
+       nosGanhamos = true; 
+      }
+    });
+  }
+
+  void _decrementCounterThem() {
+    setState(() {
+      if (_counter2 > 0) {
+      _counter2--;
+      }
+    });
+  }
+
 
   void _resetCounter() {
     setState(() {
@@ -59,32 +106,65 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Spacer(),
             Text(
               'Nós',
+              style: TextStyle(fontWeight: FontWeight.bold)
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headline1,
             ),
-            FlatButton(
-              onPressed: _incrementCounterUs,
-              child: Icon(Icons.add),
-            ),
+            Row(children: <Widget>[
+            Spacer(),
+              FloatingActionButton(
+                onPressed: _incrementCounterUs,
+                child: Icon(Icons.arrow_upward),
+              ),
+              FloatingActionButton(
+                onPressed: _incrementTrucoUs,
+                child: Text('Truco'),
+              ),
+              FloatingActionButton(
+                onPressed: _decrementCounterUs,
+                child: Icon(Icons.arrow_downward),
+              ),
+            Spacer(),
+            ],),
+            Spacer(),
             Text(
               'Eles',
+              style: TextStyle(fontWeight: FontWeight.bold)
             ),
             Text(
               '$_counter2',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headline1,
             ),
-            FlatButton(
-              onPressed: _incrementCounterThem,
-              child: Icon(Icons.add),
-            ),
+            Row(children: <Widget>[
+            Spacer(),
+              FloatingActionButton(
+                backgroundColor: Colors.red,
+                onPressed: _incrementCounterThem,
+                child: Icon(Icons.arrow_upward),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.red,
+                onPressed: _incrementTrucoThem,
+                child: Text('Truco'),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.red,
+                onPressed: _decrementCounterThem,
+                child: Icon(Icons.arrow_downward),
+              ),
+            Spacer(),
+            ],),
+            Spacer(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
         onPressed: _resetCounter,
         tooltip: 'novo jogo',
         child: Icon(Icons.autorenew),
